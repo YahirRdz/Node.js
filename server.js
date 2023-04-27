08cc642ec3be6fc4ea6de3f3df98ca114c27e871
YahirRdz <71789382+YahirRdz@users.noreply.github.com>
2023-04-26 19:33:20 -0600

    Midelware complete sect 7

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
+
+//custom middleware logger
+
+app.use(logger);
+
+//cross origin resouse sharing
+const whitelist = ["https://www.google.com", "http://localhost:3500"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed to access"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   //   res.sendFile("./views/index.html", { root: __dirname });
@@ -15,10 +42,6 @@ app.get("/old-page(.html)?", (req, res) => {
   //   res.sendFile("./views/index.html", { root: __dirname });
   res.redirect(301, "/new-page.html");
 });
-
-// app.get("/*", (req, res) => {
-//   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-// });
 
 //route handlers
 app.get(
@@ -48,5 +71,20 @@ const three = (req, res) => {
 };
 
 app.get("/chain(.html)?", [one, two, three]);
+
+//app.use('/)
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
